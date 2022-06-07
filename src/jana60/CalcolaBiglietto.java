ad74f0c4e39aa70d36c4604ff36fe962c861e9f7
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-07 17:21:48 +0200

    Modificato e perfezionato codice, esercizio terminato

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -6,43 +6,43 @@ public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Dichiarazioni variabili
+
+		// Dichiarazioni variabili
 		int kilometriDaPercorrere;
-		int et‡Passeggero;
+		int etaPasseggero;
 		double costoTratta;
-		double prezzoScontatoGiovani;
-		double prezzoScontatoAnziani;
-		
+
 		Scanner scan = new Scanner(System.in);
-		
-		//Inizio immissione dati
+
+		// Inizio immissione dati
 		System.out.print("Inserisci la tua Et‡: ");
-		et‡Passeggero = scan.nextInt();
+		etaPasseggero = scan.nextInt();
 		System.out.print("Inserisci i kilometri che vuoi percorrere: ");
 		kilometriDaPercorrere = scan.nextInt();
-		
-		//Inizio calcolo costo kilometri
+
+		// Inizio calcolo costo kilometri
 		costoTratta = kilometriDaPercorrere * 0.21;
 		System.out.println("Il costo della tratta scelta Ë di: " + costoTratta + "Ä");
-		
-		//Inizio calcolo sconto
-		prezzoScontatoGiovani = costoTratta / 100 * 20 ;
-		prezzoScontatoAnziani = costoTratta / 100 * 40 - costoTratta;
-		
-		//Inizio sequenza per identificazione sconto
-		if (et‡Passeggero<18) { System.out.println("Complimenti, hai diritto ad uno sconto del 20%! il prezzo scontato Ë di: " + prezzoScontatoGiovani + "Ä");
-			
-		} else if (et‡Passeggero>65) { System.out.println("Complimenti, hai diritto ad uno sconto del 40%! il prezzo scontato Ë di: " + prezzoScontatoAnziani + "Ä"); 
-			
+
+		// Inizio calcolo sconto
+		double prezzoScontato = costoTratta;
+
+		// Inizio sequenza per identificazione sconto
+		if (etaPasseggero < 18) {
+			prezzoScontato = costoTratta - costoTratta / 100 * 20;
+			System.out.println("Complimenti hai diritto ad un sconto del 20%, prezzo finale: ");
+
+		} else if (etaPasseggero > 65) {
+			prezzoScontato = costoTratta - costoTratta / 100 * 40;
+			System.out.println("Complimenti hai diritto ad un sconto del 40%, prezzo finale: ");
+
+		} else {
+			System.out.println("Mi dispiace non hai diritto a nessuno sconto prezzo finale: ");
+
 		}
-		else {
-			System.out.println("Mi dispiace non hai diritto a nessuno sconto");
- 
-		
+		System.out.println(prezzoScontato + "Ä");
+
 		scan.close();
-		}
-		
 	}
 
 }
