06b7858c29c92e7b6b2d6d09ba04aec8d09d537f
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-07 14:44:26 +0200

    Inseriti calcoli sconti e condizionali per calcolo sconto

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -7,20 +7,41 @@ public class CalcolaBiglietto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Dichiarazioni variabili
 		int kilometriDaPercorrere;
 		int et‡Passeggero;
 		double costoTratta;
+		double prezzoScontatoGiovani;
+		double prezzoScontatoAnziani;
+		
 		Scanner scan = new Scanner(System.in);
 		
+		//Inizio immissione dati
 		System.out.print("Inserisci la tua Et‡: ");
 		et‡Passeggero = scan.nextInt();
 		System.out.print("Inserisci i kilometri che vuoi percorrere: ");
 		kilometriDaPercorrere = scan.nextInt();
+		
+		//Inizio calcolo costo kilometri
 		costoTratta = kilometriDaPercorrere * 0.21;
-		System.out.print("Il costo della tratta scelta Ë di: " + costoTratta + "Ä");
-
+		System.out.println("Il costo della tratta scelta Ë di: " + costoTratta + "Ä");
+		
+		//Inizio calcolo sconto
+		prezzoScontatoGiovani = costoTratta / 100 * 20 ;
+		prezzoScontatoAnziani = costoTratta / 100 * 40 - costoTratta;
+		
+		//Inizio sequenza per identificazione sconto
+		if (et‡Passeggero<18) { System.out.println("Complimenti, hai diritto ad uno sconto del 20%! il prezzo scontato Ë di: " + prezzoScontatoGiovani + "Ä");
+			
+		} else if (et‡Passeggero>65) { System.out.println("Complimenti, hai diritto ad uno sconto del 40%! il prezzo scontato Ë di: " + prezzoScontatoAnziani + "Ä"); 
+			
+		}
+		else {
+			System.out.println("Mi dispiace non hai diritto a nessuno sconto");
+ 
 		
 		scan.close();
+		}
 		
 	}
 
